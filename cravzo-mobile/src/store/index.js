709e3900733552ea34582b8a422f5cd900c39769
raushanpandeyy <raushanpandey894@gmail.com>
2026-07-05 16:22:28 +0530

    fix store: fallback to localStorage when MMKV unavailable on web

--- cravzo-mobile/src/store/index.js
+++ cravzo-mobile/src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { Platform } from "react-native";
 import { createMMKV } from "react-native-mmkv";
 
 import userReducer from "./slices/userSlice";
@@ -7,21 +8,23 @@ import cartReducer from "./slices/cartSlice";
 import foodReducer from "./slices/foodSlice";
 import vendorReducer from "./slices/vendorSlice";
 
+const isWeb = Platform.OS === "web";
 
-const storageInstance = createMMKV();
-
+const mmkv = isWeb ? null : createMMKV();
 
 const reduxStorage = {
   setItem: (key, value) => {
-    storageInstance.set(key, value);
+    if (isWeb) { localStorage.setItem(key, value); return Promise.resolve(true); }
+    mmkv.set(key, value);
     return Promise.resolve(true);
   },
   getItem: (key) => {
-    const value = storageInstance.getString(key);
+    const value = isWeb ? localStorage.getItem(key) : mmkv.getString(key);
     return Promise.resolve(value);
   },
   removeItem: (key) => {
-    storageInstance.delete(key);
+    if (isWeb) { localStorage.removeItem(key); return Promise.resolve(true); }
+    mmkv.delete(key);
     return Promise.resolve(true);
   },
 };
